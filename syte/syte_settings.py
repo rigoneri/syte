eeadeff93ff4ab1554ca4dcc9d2bb42f22bfa7fd
arnonate <hello@natearnold.me>
2012-06-18 21:13:47 -0500

    lol

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -59,6 +59,6 @@ if DEPLOYMENT_MODE == 'prod':
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = 'http://natearnold.me/'
+    SITE_ROOT_URI = 'http://arnonate.herokuapp.com/'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
