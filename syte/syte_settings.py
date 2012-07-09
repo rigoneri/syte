91a8c04c16e19cfbd4e609d33be484c26ef5c8f1
junny <kiddo.mk@gmail.com>
2012-07-09 19:19:12 +0100

    fix space issue

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -65,6 +65,6 @@ if DEPLOYMENT_MODE == 'dev':
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = 'http://junseki.herokuapp.com/ '
+    SITE_ROOT_URI = 'http://junseki.herokuapp.com/'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
