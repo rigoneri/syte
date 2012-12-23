40ca4c6c3f6993985d1570e21d247536c3c3d082
Gordon Koo <gordon@gordonkoo.com>
2012-12-23 11:22:08 -0800

    add wordpress support

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -3,11 +3,16 @@
 DEPLOYMENT_MODE = 'dev'
 COMPRESS_REVISION_NUMBER = '1.0'
 
+BLOG_PLATFORM = '["wordpress" or "tumblr", without the double quotes]'
 
 #Blog Integration: Tumblr
 TUMBLR_BLOG_URL = '[ENTER TUMBLR BLOG URL] ex. rigoneri.tumblr.com'
 TUMBLR_API_URL = 'http://api.tumblr.com/v2/blog/{0}'.format(TUMBLR_BLOG_URL)
 TUMBLR_API_KEY = '[ENTER TUMBLR API KEY HERE, SEE TUMBLR SETUP INSTRUCTIONS]'
+
+#Blog Integration: Wordpress
+WORDPRESS_BLOG_URL = '[ENTER WORDPRESS BLOG URL] ex. gordonkoo.wordpress.com'
+WORDPRESS_API_URL = 'https://public-api.wordpress.com/rest/v1/sites/{0}'.format(WORDPRESS_BLOG_URL)
 
 #RSS Feed Integration: (by default use Tumbrl rss feed)
 RSS_FEED_ENABLED = True
