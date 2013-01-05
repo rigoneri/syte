ea4b7521b2bf4f7b010d8565568c75136a7f1c28
Hassan Khan <me@hassankhan.me>
2013-01-05 02:38:19 +0000

    Fixed new variable name.

--- syte/views/rss.py
+++ syte/views/rss.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def rss(request):
 	r = requests.get('{0}'.format(settings.RSS_FEED_URL))
-	new_contents = r.text.encode('utf-8').replace(settings.TUMBLR_BLOG_URL, settings.SITE_ROOT_URI[7:-1])
+	new_content = r.text.encode('utf-8').replace(settings.TUMBLR_BLOG_URL, settings.SITE_ROOT_URI[7:-1])
 
 	return HttpResponse(content=new_content,
 						status=r.status_code,
