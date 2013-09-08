708880be3622756ff0e7d46a7ee8cedd39ffc746
Rodrigo Neri <rodrigo@rigoneri.com>
2013-09-08 14:05:19 -0500

    Improved Linkedin Integration Visual styles. #228

--- syte/static/js/components/linkedin.js
+++ syte/static/js/components/linkedin.js
@@ -7,43 +7,30 @@ function setupLinkedin(url, el) {
     return;
   }
 
-  var params = url.attr('path').split('/').filter(function(w) {
-      if (w.length)
-          return true;
-      return false;
-  })
+  var spinner = new Spinner(spin_opts).spin();
+  $('#linkedin-link').append(spinner.el);
 
-  if (params.length == 0) {
+  require(["json!/linkedin/", "text!templates/linkedin-view.html"],
+    function(linkedin_data, linkedin_view) {
+        if (linkedin_data.error) {
+            window.location = href;
+            return;
+        }
 
-     var spinner = new Spinner(spin_opts).spin();
-     $('#linkedin-link').append(spinner.el);
+        var template = Handlebars.compile(linkedin_view);
 
-     require(["json!/linkedin/", "text!templates/linkedin-view.html"],
-        function(linkedin_data, linkedin_view) {
-            if (linkedin_data.error) {
-                window.location = href;
-                return;
+        linkedin_data.profile['profile_url'] = 'http://linkedin.com/profile/view?id=' + linkedin_data.profile['id']
+        linkedin_data.profile['numGroups'] = linkedin_data.groups['_count']
+        linkedin_data.profile['numNetworkUpdates'] = linkedin_data.network_updates['_total']
+        linkedin_data.profile['location_name'] = linkedin_data.profile['location']['name']
+
+        $(template(linkedin_data)).modal().on('hidden', function () {
+            $(this).remove();
+            if (currSelection === 'linkedin') {
+              adjustSelection('home');
             }
+        })
 
-            var template = Handlebars.compile(linkedin_view);
-
-            linkedin_data.profile['profile_url'] = 'http://www.linkedin.com/profile/view?id=' + linkedin_data.profile['id']
-            linkedin_data.profile['numGroups'] = linkedin_data.groups['_count']
-            linkedin_data.profile['numNetworkUpdates'] = linkedin_data.network_updates['_total']
-            linkedin_data.profile['location_name'] = linkedin_data.profile['location']['name']
-
-            $(template(linkedin_data)).modal().on('hidden', function () {
-                $(this).remove();
-                if (currSelection === 'linkedin') {
-                  adjustSelection('home');
-                }
-            })
-
-            spinner.stop();
-        });
-
-     return;
-  }
-
-  window.location = href;
+        spinner.stop();
+    });
 }
